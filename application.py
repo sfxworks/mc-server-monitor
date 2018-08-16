1da2eb8da7a46b98b1f1e1ead12c39c4943c6025
Samuel Walker <services@sfxworks.net>
2018-08-16 08:30:15 -0400

    Modifications
    Modified it to parse log file. Included basic template. No input yet.

--- application.py
+++ application.py
@@ -24,13 +24,17 @@ from flask import Flask, render_template, url_for, copy_current_request_context
 from random import random
 from time import sleep
 from threading import Thread, Event
+import sys
 
+#define log file in first argument
+#python application.py <log file path>
+logfile = sys.argv[1]
 
-__author__ = 'slynn'
+__author__ = 'quantomworks'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = '\xfc\xe0.\xfa\xf2\x18EG\xedo\xff[\xc7.\xd0\xfa'
+app.config['DEBUG'] = False
 
 #turn the flask app into a socketio app
 socketio = SocketIO(app)
@@ -38,50 +42,70 @@ socketio = SocketIO(app)
 #random number Generator Thread
 thread = Thread()
 thread_stop_event = Event()
-
-class RandomThread(Thread):
+        
+class LogReader(Thread):
     def __init__(self):
         self.delay = 1
-        super(RandomThread, self).__init__()
-
-    def randomNumberGenerator(self):
+        super(LogReader, self).__init__()
+    
+    def listenToLogs(self):
+        cur = 0
         """
-        Generate a random number every 1 second and emit to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
+        Tail latest.log and send the data.
+        Sort the emits based on the 3 outputs
+        INFO
+        WARN
+        ERROR
         """
-        #infinite loop of magical random numbers
-        print("Making random numbers")
+        def analyze_line(line):
+            if line.find('ERROR]') > -1:
+                print('Error:' + line)
+                socketio.emit('sterr', {'line': line}, namespace='/log')  
+                
+            elif line.find('WARN]') > -1:
+                print('Warning' + line)
+                socketio.emit('stwarn', {'line': line}, namespace='/log')
+                
+            elif line.find('INFO]') > -1:
+                print('Info' + line)
+                socketio.emit('stout', {'line': line}, namespace='/log')
+        
         while not thread_stop_event.isSet():
-            number = round(random()*10, 3)
-            print(number)
-            socketio.emit('newnumber', {'number': number}, namespace='/test')
-            sleep(self.delay)
-
+            try:
+                with open(logfile) as f:
+                    f.seek(0,2)
+                    if f.tell() < cur:
+                        f.seek(0,0)
+                    else:
+                        f.seek(cur,0)
+                    for line in f:
+                        analyze_line(line)
+                    cur = f.tell()
+            except IOError:
+                pass
     def run(self):
-        self.randomNumberGenerator()
+        self.listenToLogs()
 
 
 @app.route('/')
-def index():
-    #only by sending this page first will the client be connected to the socketio instance
-    return render_template('index.html')
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/log')
 def test_connect():
     # need visibility of the global thread object
     global thread
     print('Client connected')
 
-    #Start the random number generator thread only if the thread has not been started before.
+    #Start the log thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
-        thread = RandomThread()
+        thread = LogReader()
         thread.start()
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/log')
 def test_disconnect():
     print('Client disconnected')
 
-
+#Host and port defined at args 2 and 3 respectively
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host=sys.argv[2], port=sys.argv[3])
+    
